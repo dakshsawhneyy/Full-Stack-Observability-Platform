620d6fa95e852634007b88055d3eb14b3aa5090b
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-24 12:09:07 +0530

    Update index.js

--- Sample-Node-App/index.js
+++ Sample-Node-App/index.js
@@ -1,13 +1,55 @@
 const express = require('express')
 const {v4:uuid} = require('uuid')
 const fs = require('fs')
+const client = require('prom-client')
+const responseTime = require('response-time')
+
+// Loki
+const { createLogger, transports } = require("winston");
+const LokiTransport = require("winston-loki");
+
+// Sending data to LOKI
+const options = {
+  transports: [
+    new LokiTransport({
+      host: "http://3.111.23.106:3100"
+    })
+  ]
+};
+const logger = createLogger(options);   // we can add logs inside this logger
 
 const app = express();
+
+// This collects default metrics like ram, cpu etc
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register })
+
+// Creating Histogram of this route
+const reqResTime = new client.Histogram({
+    name: 'http_req_res_time',
+    help: 'Request Response Time',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [1,50,100,200,500,800,1000,2000]  // buckets are used to categorize the time taken
+})
+
+// Creating metric for counter
+const totalReqCounter = new client.Counter({
+    name: 'total_req',
+    help: 'Total number of requests',
+})
+
+// Custom Metrics Route
+// This will tell the requests take how much time -- apply on each request
+app.use(responseTime((req, res, time) => {
+    reqResTime.labels({ method: req.method, route: req.url, code: req.statusCode }).observe(time)
+    totalReqCounter.inc()
+}))
 
 const levels = ["info", "debug", "error", "warn", "critical"]
 
 app.get('/', (req,res) => {
     res.send("Hello Beautiful");
+    logger.error("Req came on / route")
 })
 
 app.get('/daksh', (req,res) => {
@@ -34,6 +76,7 @@ app.get('/daksh', (req,res) => {
         request_id: requestId
     }) 
     fs.appendFileSync('app.log', log + '\n')
+    logger.info(log)
 })
 
 app.get('/shubu', (req,res) => {
@@ -61,6 +104,14 @@ app.get('/shubu', (req,res) => {
     })
 
     fs.appendFileSync('app.log', log + '\n')
+    logger.info(log)
+})
+
+// Prometheus Metrics Route -- exposing the metrics
+app.get('/metrics', async (req,res) => {
+  res.setHeader('Content-Type', client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
 })
 
 app.listen(9000, () => console.log(`Listening on Port 9000`));
